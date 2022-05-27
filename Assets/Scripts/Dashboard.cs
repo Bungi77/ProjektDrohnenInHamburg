f750e68ff108d3305614908844e2bd08da366a04
Aleks Desktop <aleksandar.petrusov@studium.uni-hamburg.de>
2022-05-27 13:32:16 +0200

    Reset 27.05.

---
+++ Assets/Scripts/Dashboard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dashboard : MonoBehaviour
+{
+   public void LogoutButton()
+   {
+       Application.Quit();
+       Debug.Log("Abgemeldet"); 
+   }
+   
+   public void LiveAnsicht()
+   {
+       SceneManager.LoadScene("MainSceneDroneFlight");
+   }
+}
